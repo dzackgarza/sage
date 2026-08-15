4a6d2e78859a6ae79dc45fd85ac28c0a121438ab
Zack Garza <dzackgarza@gmail.com>
2026-08-15 20:50:39 +0800

    Use shared Sage stubs

--- src/sage/rings/complex_conversion.pyi
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-from sage.structure.element import Element
-from sage.categories.map import Map
-
-class CCtoCDF(Map):
-    def _call_(self, x: Element) -> Element:
-        ...
